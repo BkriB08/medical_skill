e94616b8c2a19df23b0466978d7c612b277277c8
BkriB08 <krisktik2008@gmail.com>
2025-04-30 03:17:43 +0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from symptoms import list_all_symptoms
-from main3 import diagnose
-from lib_gpt import get_answer_to_question_yandexGPT
-
-def YPT_symptom ():
-    symptom = get_answer_to_question_yandexGPT("какой симптом вас беспокоит?", initial_symptom_name , list_all_symptoms)
-    print (symptom)
